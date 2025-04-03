1f4ba60c3d12564bcb3170a9008cfa02d57f6628
subtle852 <subtle852@gmail.com>
2025-04-03 16:23:50 +0900

    002

---
+++ 002/002.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> lst;
+vector<int> result;
+
+vector<int> solution()
+{
+    lst.erase(unique(lst.begin(), lst.end()), lst.end());// O(N)
+}
+
+int main()
+{
+    lst = { 5, 10, 9, 12, 2, 9 };
+
+    sort(lst.begin(), lst.end(), [](int a, int b)->bool { return a > b; });// O(NlogN)
+    result = solution();
+
+    for (auto i : result)
+        cout << i << " ";
+}
+
+// erase와 unique
+// 사용자 정의 bool
