9cee6e00712e93b9ddfbaf22957e4bc18ad56ebc
subtle852 <subtle852@gmail.com>
2025-04-05 05:23:13 +0900

    002 오류 수정

--- 002/002.cpp
+++ 002/002.cpp
@@ -3,20 +3,20 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> lst;
-vector<int> result;
-
-vector<int> solution()
+vector<int>& solution(vector<int>& input)
 {
-    lst.erase(unique(lst.begin(), lst.end()), lst.end());// O(N)
+    input.erase(unique(input.begin(), input.end()), input.end());// O(N)
+
+    return input;
 }
 
 int main()
 {
-    lst = { 5, 10, 9, 12, 2, 9 };
+    vector<int> lst = { 5, 10, 9, 12, 2, 9 };
+    vector<int> result;
 
     sort(lst.begin(), lst.end(), [](int a, int b)->bool { return a > b; });// O(NlogN)
-    result = solution();
+    result = solution(lst);
 
     for (auto i : result)
         cout << i << " ";
