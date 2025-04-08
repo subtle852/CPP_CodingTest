41f1ca9343295f4761a4946a1cb21fef1c6572b2
subtle852 <subtle852@gmail.com>
2025-04-08 11:01:52 +0900

    011

---
+++ 011/011.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(const string& input)
+{
+    stack<char> s;
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        if (s.empty() == false && s.top() == input[i])
+        {
+            s.pop();
+        }
+        else
+        {
+            s.push(input[i]);
+        }
+    }
+
+    if (s.empty())
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    //string input = "baabaa";
+    string input = "cdcd";
+    cout << solution(input);
+
+}
